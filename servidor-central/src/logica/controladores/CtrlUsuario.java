ec6722c2974677b80c40c8d6be04bfbd318aac68
Guido Dinello <guido.dinello@gmail.com>
2022-08-22 23:01:34 -0300

    implementada altaUsuario en CtrlUsuario

--- servidor-central/src/logica/controladores/CtrlUsuario.java
+++ servidor-central/src/logica/controladores/CtrlUsuario.java
@@ -1,6 +1,21 @@
 package logica.controladores;
 import logica.interfaces.ICtrlUsuario;
 
-public class CtrlUsuario implements ICtrlUsuario {
+import java.util.Date;
 
+public class CtrlUsuario implements ICtrlUsuario {
+	
+	public void altaUsuario(String nickname, String email, String nombre, String apellido, Date fechaNac, tipoUsuario tipo, String nacionalidad, String descripcion, String sitioWeb) {
+		HandlerUsuarios hu = HandlerUsuarios.getInstance();
+		Usuario usr;
+		if (tipo == tipoUsuario.turista) {
+			usr = new Turista(nickname, email, nombre, apellido, fechaNac, nacionalidad);
+			hu.agregarTurista(usr);
+		} else {
+			usr = new Proveedor(nickname, email, nombre, apellido, fechaNac, descripcion, sitioWeb);
+			hu.agregarProveedor(usr);
+		}
+		// cambiar hu.agregarUsuario
+		hu.add(usr);
+	}
 }
