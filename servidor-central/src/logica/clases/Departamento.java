c60a450fb45d6d3bdbea5985fc08867c2a234cb3
eber.manuel.rodriguez <eber.manuel.rodriguez@fing.edu.uy>
2022-08-23 15:54:04 -0300

    Alta de departamento, creacion del handler y de la exception ya existe.

--- servidor-central/src/logica/clases/Departamento.java
+++ servidor-central/src/logica/clases/Departamento.java
@@ -1,9 +1,22 @@
 package logica.clases;
 
+//importaciones
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Departamento {
 	private String nombre;
 	private String URL;
+	private Map<String, ActividadTuristica> actividades;
 	
+	public Departamento(String nombre, String URL) {
+		this.nombre = nombre;
+		this.URL = URL;
+		this.actividades = new HashMap<String, ActividadTuristica>();
+	}
 	
-
+	public String getNombre() {
+		return this.nombre;
+	}
 }
