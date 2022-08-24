acbaa64785a44d68b7e47bc3be253af310e997e8
Guido Dinello <guido.dinello@gmail.com>
2022-08-24 14:32:08 -0300

    Cambios a Handler Usuarios llaves independientes

--- servidor-central/src/logica/handlers/HandlerUsuarios.java
+++ servidor-central/src/logica/handlers/HandlerUsuarios.java
@@ -1,10 +1,12 @@
 package logica.handlers;
 
 import java.util.List;
+
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -14,16 +16,56 @@ import logica.clases.Proveedor;
 import excepciones.NoExisteUsuario;
 import excepciones.YaExisteException;
 
-public class HandlerUsuarios{
+public class HandlerUsuarios {
+	
+	private class DoubleKeyedHashMap <T> {
+		private Map<String, T> hashByNickname;
+		private Map<String, T> hashByEmail;
+		
+		public DoubleKeyedHashMap() {
+			hashByNickname = new HashMap<String, T>();
+			hashByEmail = new HashMap<String, T>();
+		}
+		
+		public void add(String nickname, String email, T usr) throws YaExisteException {
+			T repeated_nick_value = hashByNickname.get(usr);
+			T repeated_email_value = hashByEmail.get(usr);
+			if (repeated_nick_value != null) 
+				throw new YaExisteException("el nickname " + nickname + " ya existe");
+			if (repeated_email_value != null) 
+				throw new YaExisteException("el email " + email + " introducidos ya existe");
+			hashByNickname.put(nickname, usr);
+			hashByEmail.put(email, usr);
+		}
+		
+		public Collection<T> values() {
+			return hashByNickname.values();
+		}
+		
+		public T getByNickname(String nick) throws NoExisteUsuario {
+			T usr = hashByNickname.get(nick);
+			if (usr != null)
+				return usr;
+			throw new NoExisteUsuario("No existe usuario con nickname: " + nick);
+		}
+		
+		public T getByEmail(String e) throws NoExisteUsuario {
+			T usr = hashByEmail.get(e);
+			if (usr != null)
+				return usr;
+			throw new NoExisteUsuario("No existe usuario con email: " + e);
+		}
+		
+	}
 	
 	private static HandlerUsuarios instance = null;
 
-	Map<List<String>, Proveedor> proveedores;
-	Map<List<String>, Turista> turistas;
+	DoubleKeyedHashMap<Proveedor> proveedores;
+	DoubleKeyedHashMap<Turista> turistas;
 	
 	private HandlerUsuarios() {
-		proveedores = new HashMap<List<String>, Proveedor>();
-		turistas = new HashMap<List<String>, Turista>();
+		proveedores = new DoubleKeyedHashMap<Proveedor>();
+		turistas = new DoubleKeyedHashMap<Turista>();
 	}
 
 	public static HandlerUsuarios getInstance() {
@@ -31,75 +73,53 @@ public class HandlerUsuarios{
             instance = new HandlerUsuarios();
         return instance;
 	}
-	
-	private <T> boolean uniqueKey(List<String> key, Map<List<String>, T> collection) throws YaExisteException{
-		String nickname = key.get(0);
-		String email = key.get(1);
 
-        Iterator<Entry<List<String>, T>> iterator = collection.entrySet().iterator();
-        
-        boolean nickname_flag = false;
-        boolean email_flag = false;
-
-        while (iterator.hasNext()) {
-            Entry<List<String>, T> entry = iterator.next();
-            if (entry.getKey().get(0) == nickname)
-            	nickname_flag = true;
-            if (entry.getKey().get(1) == email)
-            	email_flag = true;
-            if (email_flag && nickname_flag)
-            	throw new YaExisteException("el nickname y el email introducidos ya existen");
-            else {
-            	if (nickname_flag)
-            		throw new YaExisteException("el nickname " + nickname + " ya existe");
-            	else 
-            		throw new YaExisteException("el email " + email + " introducidos ya existe");
-            }
-        }
-        return true;
+	public void agregarTurista(Turista t) throws YaExisteException {
+		turistas.add(t.getNickname(), t.getEmail(), t);
 	}
-	
-	private <T extends Usuario> void add(T usr, Map<List<String>, T> collection) throws YaExisteException {
-		List<String> keys = List.of(usr.getNickname(), usr.getEmail());
-		if (! uniqueKey(keys, collection)) {
-			collection.put(keys, usr);
+	public void agregarProveedor(Proveedor p) throws YaExisteException {
+		proveedores.add(p.getNickname(), p.getEmail(), p);
+	}
+	public void agregarUsuario(Usuario u) throws YaExisteException {
+		if (u instanceof Proveedor) {
+			Proveedor p = (Proveedor)u;
+			proveedores.add(p.getNickname(), p.getEmail(), p);
+		} else {
+			Turista t = (Turista)u;
+			turistas.add(t.getNickname(), t.getEmail(), t);
 		}
-	}
 
-	public void agregarTurista(Turista turista) throws YaExisteException {
-		add(turista, turistas);
-	}
-
-	public void agregarProveedor(Proveedor prov) throws YaExisteException {
-		add(prov, proveedores);
 	}
 	
-	public Usuario getUsuario(List<String> key) throws NoExisteUsuario {
-		Proveedor p = proveedores.get(key);
+	public Usuario getUsuarioByNickname(String n) throws NoExisteUsuario {
+		Proveedor p = proveedores.getByNickname(n);
 		if (p != null)
 			return (Usuario)p;
-		Turista t = turistas.get(key);
-		if (t != null)
-			return (Usuario)t;
-		throw new NoExisteUsuario("No existe usuario con nickname: " + key.get(0) + " e email: " + key.get(1));
+		return (Usuario)turistas.getByNickname(n);
+	}
+	public Usuario getUsuarioByEmail(String n) throws NoExisteUsuario {
+		Proveedor p = proveedores.getByEmail(n);
+		if (p != null)
+			return (Usuario)p;
+		return (Usuario)turistas.getByEmail(n);
 	}
 	
-	public Turista getTurista(String nickname, String email) throws NoExisteUsuario {
-		return (Turista)getUsuario(List.of(nickname, email));
-	}
-	
-	public Proveedor getProveedor(String nickname, String email) throws NoExisteUsuario {
-		return (Proveedor)getUsuario(List.of(nickname, email));
-	}
+	public Turista getTuristaByNickname(String n) throws NoExisteUsuario { return turistas.getByNickname(n); }
+	public Proveedor getProveedorByNickname(String n) throws NoExisteUsuario { return proveedores.getByNickname(n); }
+	public Turista getTuristaByEmail(String e) throws NoExisteUsuario { return turistas.getByEmail(e); }
+	public Proveedor getProveedorByEmail(String e) throws NoExisteUsuario { return proveedores.getByEmail(e); }
 
 	public Set<Turista> listarTuristas() {
 		Set<Turista> res = new HashSet<Turista>(turistas.values());
 		return res;
 	}
-	
+	public Set<Proveedor> listarProveedores() {
+		Set<Proveedor> res = new HashSet<Proveedor>(proveedores.values());
+		return res;
+	}
 	public Set<Usuario> listarUsuarios(){
 		Set<Usuario> res = new HashSet<Usuario>(turistas.values());
-		res.addAll(this.proveedores.values());
+		res.addAll(proveedores.values());
 		return res;
 	}
 }
