e27e02705abe662e5dee156d85744b4942d3d53c
Francisco Mauri <fmauri@fing.edu.uy>
2022-08-24 01:19:42 -0300

    ActividadTuristica

--- servidor-central/src/logica/clases/ActividadTuristica.java
+++ servidor-central/src/logica/clases/ActividadTuristica.java
@@ -1,5 +1,17 @@
 package logica.clases;
 
+import java.util.GregorianCalendar;
+
 public class ActividadTuristica {
+	private String nombre;
+	private String descripcion;
+	private int duracionHs;
+	private float costoPorTurista;
+	private String nombreCiudad;
+	private GregorianCalendar fechaAlta;
+	
+	public String getNombre() {
+		return this.nombre;
+	}
 
 }
