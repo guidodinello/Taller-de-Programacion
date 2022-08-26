9149ac10aac60c5a76161d4c0c1b6649f37a7e35
Guido Dinello <guido.dinello@gmail.com>
2022-08-26 01:23:37 -0300

    ctrl usuario test

--- servidor-central/src/test/ctrlUsuario.java
+++ servidor-central/src/test/ctrlUsuario.java
@@ -7,15 +7,13 @@ import java.util.GregorianCalendar;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import logica.controladores.CtrlUsuario;
 import logica.interfaces.Fabrica;
 import logica.interfaces.ICtrlUsuario;
 import datatypes.tipoUsuario;
-import excepciones.NoExisteUsuario;
 import excepciones.YaExisteException;
 import datatypes.DTUsuario;
 
-public class altaUsuario {
+public class ctrlUsuario {
 	private static ICtrlUsuario controladorUsuario;
 	
 	@BeforeAll
@@ -25,13 +23,40 @@ public class altaUsuario {
 	}
 	
 	@Test
-	void testRegistrarProveedorOK() {
+	void testAltaProveedorOK() {
 		String nick = "prov_nick";
 		String em = "prov_email";
 		String nom = "prov_nombre";
 		String ap = "prov_apellido";
-		GregorianCalendar fecha = new GregorianCalendar();
+		GregorianCalendar fecha = new GregorianCalendar(2022, 12, 1);
 		tipoUsuario tipo = tipoUsuario.proveedor;
+		String nacio = "uruguaya";
+		String desc = "un proveedor";
+		String sitioweb = "google.com";
+		
+		try {
+			controladorUsuario.altaUsuario(nick, em, nom, ap, fecha, tipo, nacio, desc, sitioweb);
+			DTUsuario du = controladorUsuario.getInfoBasicaUsuario(nick);
+			
+			assertEquals(du.getNickname(), nick);
+			assertEquals(du.getEmail(), em);
+			assertEquals(du.getFechaNac(), fecha);
+			assertEquals(du.getNombre(), nom);
+			assertEquals(du.getApellido(), ap);
+		} catch (YaExisteException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		};
+	}
+	
+	@Test
+	void testAltaTuristaOK() {
+		String nick = "tur_nick";
+		String em = "tur_email";
+		String nom = "tur_nombre";
+		String ap = "tur_apellido";
+		GregorianCalendar fecha = new GregorianCalendar(2022, 12, 1);
+		tipoUsuario tipo = tipoUsuario.turista;
 		String nacio = "uruguaya";
 		String desc = "un proveedor";
 		String sitioweb = "google.com";
@@ -52,12 +77,12 @@ public class altaUsuario {
 	}
 
 	@Test
-	void testRegistrarProveedorRepetido() {
+	void testProveedorRepetido() {
 		String nick = "prov_nick";
 		String em = "prov_email";
 		String nom = "prov_nombre";
 		String ap = "prov_apellido";
-		GregorianCalendar fecha = new GregorianCalendar();
+		GregorianCalendar fecha = new GregorianCalendar(2022, 12, 1);
 		tipoUsuario tipo = tipoUsuario.proveedor;
 		String nacio = "uruguaya";
 		String desc = "un proveedor";
@@ -69,19 +94,22 @@ public class altaUsuario {
 			fail(e.getMessage());
 			e.printStackTrace();
 		};
-		//esta es la prueba
 		assertThrows(YaExisteException.class, ()->{controladorUsuario.altaUsuario(nick, em, nom, ap, fecha, tipo, nacio, desc, sitioweb);});	
 	}
 
 	@Test
-	void testVerInfoUsuario() {
-		fail("Not yet implemented");
+	void ingresarInscripcionOK() { 
+		String nick = "tur_nick";
+		String salida = "salida_test";
+		int cant = 5;
+		GregorianCalendar fecha = new GregorianCalendar(2022, 12, 1);
+		
+		controladorUsuario.ingresarInscripcion(nick, salida, cant, fecha); 
 	}
-
-	@Test
-	void testGetUsuarios() {
-		fail("Not yet implemented");
-	}
-
 
 }
+
+//	@Test
+//	void testGetUsuarios() {
+//		fail("Not yet implemented");
+//	}
