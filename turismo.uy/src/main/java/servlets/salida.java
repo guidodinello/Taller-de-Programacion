cc5c5c865872efc25ab84ac6c5d49041daa2f8d1
Cristian Gonzalez <cristiangdev@gmail.com>
2022-10-09 08:34:12 -0300

    consultaSalida

---
+++ turismo.uy/src/main/java/servlets/salida.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.logica.interfaces.ICtrlActividad;
+import model.logica.interfaces.Fabrica;
+
+import model.datatypes.DTSalida;
+
+@WebServlet("/salida")
+
+public class salida extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public salida() {
+		super();
+	}
+	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException ,IOException {
+		ICtrlActividad ctrlActividad = Fabrica.getInstance().getICtrlActividad();
+		String name = request.getParameter("name");
+		DTSalida salidaT = ctrlActividad.getInfoCompletaSalida(name);
+		
+		request.setAttribute("salida", salidaT);
+		
+		request.getRequestDispatcher("/WEB-INF/altaUsuario/consultaSalida.jsp").
+			forward(request, response);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+}
