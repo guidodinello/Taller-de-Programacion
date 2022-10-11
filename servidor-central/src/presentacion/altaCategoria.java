ecdd5ab8703d01c4f2dae87045eb99bb2ef4c021
Francisco Mauri <franciscomauricanale@gmail.com>
2022-10-11 12:48:03 -0300

    para pullear

--- servidor-central/src/presentacion/altaCategoria.java
+++ servidor-central/src/presentacion/altaCategoria.java
@@ -26,6 +26,7 @@ public class altaCategoria extends JInternalFrame {
 	
 	private JTextField textField;
 	public altaCategoria(ICtrlActividad ica) {
+		setTitle("Alta de categoria");
 		setClosable(true);
 		setIconifiable(true);
 		setMaximizable(true);
