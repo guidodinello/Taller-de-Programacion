39c54d667e1f415d663f7c2ca36e2d858b28332d
Guido Dinello <guido.dinello@gmail.com>
2022-10-12 14:03:55 -0300

    servlet index

---
+++ turismo.uy/src/main/java/servlets/index.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datosDePrueba.CargarDatosDePrueba;
+import model.logica.interfaces.ICtrlActividad;
+import model.logica.interfaces.ICtrlUsuario;
+import model.logica.interfaces.Fabrica;
+
+/**
+ * Servlet implementation class index
+ */
+@WebServlet("/")
+public class index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public index() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    CargarDatosDePrueba cdp = new CargarDatosDePrueba();
+        Fabrica fabrica = Fabrica.getInstance();
+        ICtrlUsuario ICU = fabrica.getICtrlUsuario();
+        ICtrlActividad ICA = fabrica.getICtrlActividad();
+        cdp.cargarDatos(ICU, ICA);
+        // algo para generar la vista de adecuada de home.jsp
+        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
