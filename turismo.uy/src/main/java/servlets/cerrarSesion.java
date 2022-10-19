bf04bd1fab07700dea38ef1743ea1532521f08b1
Guido Dinello <guido.dinello@gmail.com>
2022-10-19 20:04:50 -0300

    cerrarSesion

--- turismo.uy/src/main/java/servlets/cerrarSesion.java
+++ turismo.uy/src/main/java/servlets/cerrarSesion.java
@@ -27,7 +27,7 @@ public class cerrarSesion extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	    request.getSession().invalidate();
-	    request.getRequestDispatcher("/index.jsp").forward(request, response);
+	    request.getRequestDispatcher("/index").forward(request, response);
 	}
 
 	/**
