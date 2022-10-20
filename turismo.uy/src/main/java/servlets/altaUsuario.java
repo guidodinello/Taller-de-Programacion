46a0db0ea4b07f0ae49f895fb62d09bf168096d6
eber.manuel.rodriguez <eber.manuel.rodriguez@fing.edu.uy>
2022-10-20 14:13:57 -0300

    Para pullear

--- turismo.uy/src/main/java/servlets/altaUsuario.java
+++ turismo.uy/src/main/java/servlets/altaUsuario.java
@@ -87,15 +87,15 @@ public class altaUsuario extends HttpServlet {
 		String fotoDir = ""; //para guardar la direccion;
 		
 		if(foto.getInputStream() != null) {
-		    System.out.println(foto.getInputStream());
+		    
 			if(!extencionValida(foto.getSubmittedFileName()).isEmpty()) {
 			    inputStreamFoto = guardarImgBin(foto, request);
 			    fotoBin = inputStreamFoto.readAllBytes();
 				fotoDir = "imagen?nick="+nick;
-				System.out.println("entra if");
+				
 			}else {
 			    fotoDir = "media/imagenes/usuarioPerfil.png";
-			    System.out.println("entra else");
+			    
 			}
 		}
 		try {
