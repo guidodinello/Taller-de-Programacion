8fc176cca3052c2707a7ef7b01d79c6d4a2fe16c
Cristian Gonzalez <cristiangdev@gmail.com>
2022-10-20 14:41:05 -0300

    altaActividad funcionando

--- turismo.uy/src/main/java/servlets/altaActividad.java
+++ turismo.uy/src/main/java/servlets/altaActividad.java
@@ -35,17 +35,21 @@ public class altaActividad extends HttpServlet {
         HttpSession session = request.getSession();
         DTProveedor prov = (DTProveedor) session.getAttribute("usuario_logueado");
         
-        String departamento = request.getParameter("Departamento");
-        String nombre = request.getParameter("Nombre");
-        String descripcion = request.getParameter("Descripcion");
+        String departamento =(String) request.getParameter("Departamento");
+        String nombre =(String) request.getParameter("Nombre");
+        String descripcion =(String) request.getParameter("Descripcion");
         int duracion = Integer.parseInt(request.getParameter("Duracion"));
         float costo = Float.parseFloat(request.getParameter("Costo"));
-        String ciudad = request.getParameter("Ciudad");
-        String img = request.getParameter("Img");
+        String ciudad =(String) request.getParameter("Ciudad");
+        String img =(String) request.getParameter("Img");
+        
         Set<String> categorias = new HashSet<String>(Arrays.asList(request.getParameterValues("Categorias")));
         
         try {
+            if(img == null)
+                img = "";
             ctrlAct.altaActividadTuristica(departamento, nombre, descripcion, duracion, costo, ciudad, prov.getNickname(), new GregorianCalendar(), img, categorias, estadoActividad.agregada);
+            request.getRequestDispatcher("/index").forward(request, response);
         } catch(YaExisteException e) {
             e.printStackTrace();
             request.setAttribute("fail", true);
