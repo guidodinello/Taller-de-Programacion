91268e57bc47218934b9173d313e57fc79f4bd70
Francisco Mauri <franciscomauricanale@gmail.com>
2022-10-23 14:01:57 -0300

    swim alta cat y act

--- servidor-central/src/presentacion/altaCategoria.java
+++ servidor-central/src/presentacion/altaCategoria.java
@@ -88,12 +88,14 @@ public class altaCategoria extends JInternalFrame {
 				
 				IActividad.altaCategoria(nombreCategoria);
 				limpiarFormulario();
+				JOptionPane.showMessageDialog(this,
+						"Categoria creada con exito", "Exito", JOptionPane.INFORMATION_MESSAGE);
+				setVisible(false);
 			
 			} catch(YaExisteException e) {
 				JOptionPane.showMessageDialog(this, e.getMessage(), "Alta de Categoria ", JOptionPane.ERROR_MESSAGE);
 			}
 			
-			setVisible(false);
 		}
 	}
 	
