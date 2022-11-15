161cbbc31687a18278dbaa2ecc4cd091b3022ad0
Cristian Gonzalez NuÃ±ez <cristian.gonzalez@fing.edu.uy>
2022-11-15 09:15:13 -0300

    Script para el empaquetado jar y war

---
+++ script.sh
@@ -0,0 +1,8 @@
+PROJECT_ABSOLUT_PATH=/ens/devel01/tpgr14/git/tpgr142/
+cd $PROJECT_ABSOLUT_PATH
+cd servidor-central/
+mvn clean install
+cd ../turismo.uy/
+mvn clean install
+cd ../servidor-movil/
+mvn clean install
